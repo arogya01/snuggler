048e654950703d77ae4173a286352f40ce28edb2
arogya bichpuria <arogya.bichpuria@bfhl.in>
2023-08-17 16:23:04 +0530

    added a post request for /message

--- server.js
+++ server.js
@@ -4,9 +4,20 @@ const app = express();
 
 const PORT = 3000; 
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     console.log('the body is',req.body);
     res.send('Hello World');
+});
+
+app.post('/message',(req,res)=>{
+    console.log('/message body is');
+    console.dir(req.body);
+    res.json({
+        messageRecieved: true
+    });
 });
 
 
